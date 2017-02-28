5a285bda272b452487038d60c8a074c1bf676e1d
Bushemi <@Igor>
2017-02-28 19:49:59 +0200

    File generator for calculator application

---
+++ src/fileGenerator/FileGenerator.java
@@ -0,0 +1,48 @@
+package fileGenerator;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Random;
+
+import calculator.CalculatorService;
+
+public class FileGenerator {
+private static StringBuilder sb;
+private static Random r = new Random();
+private static String[] array = {"+","-","*","/"};
+private static int MAX_COUNT = 10_000_000;
+	public static void main(String[] args) {
+		sb= new StringBuilder();
+
+		for(int i=0;i<MAX_COUNT;i++){
+			genStr();
+			
+		}
+		save();
+	}
+	private static void genStr(){
+		sb.append(String.valueOf(r.nextInt(10))+array[r.nextInt(4)]+String.valueOf(r.nextInt(10))+"\n");
+	}
+	private static boolean save(){
+		
+		boolean result = false;
+		
+		
+		System.out.println("save1");
+		try(Writer w = new FileWriter("Math.txt")){
+			w.write(String.valueOf(sb));
+			result=true;
+			System.out.println("success");
+		}catch(IOException e){
+			System.out.println("fail");
+			result=false;
+			throw new RuntimeException("ошибка записи",e);
+		} finally{
+			return result;
+		}
+		
+		
+	}
+
+}
